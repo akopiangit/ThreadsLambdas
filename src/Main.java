aae53b7245eb79e111d16a465b6a516d911a3ae6
AkopyanAG <AkopyanAG@intech.rshb.ru>
2020-11-24 16:33:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Callable task = () -> {
+			return 2;
+		};
+
+		FutureTask<String> future = new FutureTask<>(task);
+		new Thread(future).start();
+		System.out.println(future.get());
+
+	}
+
+}
